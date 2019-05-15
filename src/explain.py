4be724ee8593c03aa67f87ecf8fa84762b268f0c
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-05-15 23:02:49 +0530

    more features

--- src/explain.py
+++ src/explain.py
@@ -1,4 +1,9 @@
 """SHAP explanations for the titanic model."""
+import io
+import base64
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 import shap
 import numpy as np
 
@@ -14,3 +19,25 @@ def shap_values_for(model, X):
     if isinstance(sv, list):
         sv = sv[1]
     return expl, sv
+
+
+def force_plot_png(model, feat_cols, X_row):
+    expl, sv = shap_values_for(model, X_row.reshape(1, -1))
+    base = expl.expected_value
+    if isinstance(base, (list, np.ndarray)):
+        base = float(np.array(base).flatten()[-1])
+    fig = plt.figure(figsize=(8, 3))
+    contribs = list(zip(feat_cols, sv[0], X_row))
+    contribs.sort(key=lambda t: abs(t[1]), reverse=True)
+    names = [c[0] for c in contribs]
+    vals = [c[1] for c in contribs]
+    colors = ["#d62728" if v < 0 else "#2ca02c" for v in vals]
+    plt.barh(names[::-1], vals[::-1], color=colors[::-1])
+    plt.title("SHAP contribution to survival probability")
+    plt.xlabel("contribution")
+    plt.tight_layout()
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png", dpi=110)
+    plt.close(fig)
+    buf.seek(0)
+    return "data:image/png;base64," + base64.b64encode(buf.getvalue()).decode("ascii")
