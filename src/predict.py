47f9a4213e9186bfdd312921653189e0142819d1
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-05-23 20:27:27 +0530

    fix bug

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,5 @@
 """Run prediction on a single passenger record."""
 import joblib
-import numpy as np
 import pandas as pd
 from src.preprocess import make_features
 
@@ -12,8 +11,10 @@ def load_artifacts(model_path):
 def predict_one(model, feat_cols, record):
     df = pd.DataFrame([record])
     df = make_features(df)
-    # ensure column order
-    df = df[feat_cols]
-    proba = model.predict_proba(df.values)[0][1]
+    # some columns might be missing on a single-record input,
+    # reindex against the saved feat_cols and zero-fill
+    df = df.reindex(columns=feat_cols, fill_value=0)
+    X = df.values.astype(float)
+    proba = model.predict_proba(X)[0][1]
     pred = int(proba >= 0.5)
     return pred, float(proba)
